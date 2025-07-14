1d0dcd6c687ed50224506a4a8d8ab33afe7c1ccf
Thanoj30 <tthanoj18@gmail.com>
2025-07-14 14:52:07 +0530

    Add files via upload

---
+++ html/1js.js
@@ -0,0 +1,16 @@
+let age = 31;
+let isStudent = true;
+
+if (age < 30) {
+    if (isStudent) {
+        console.log("Young student");
+    } else {
+        console.log("Young non-student");
+    }
+} else {
+    if (isStudent) {
+        console.log("Older student");
+    } else {
+        console.log("Older non-student");
+    }
+}
